9e6fb920f4f497d3adde49f8398e8d4598d66f6b
wallisonstorck <storck779@gmail.com>
2021-10-26 23:12:16 -0400

    Adding more routes gets

--- routes/grades.js
+++ routes/grades.js
@@ -5,14 +5,14 @@ const router = express.Router();
 
 const { readFile, writeFile } = fs;
 
-const gradeFile = "grades.json"; //Caminho do arquivo de grades
+const GRADE_FILE = "grades.json"; //Caminho do arquivo de grades
 
 /**
  * 1. Crie um endpoint para criar uma grade. Este endpoint deverá receber como parâmetros
  * os campos student, subject, type e value.
  */
 router.post("/", async (req, res) => {
-  const data = await JSON.parse(await readFile(gradeFile)); //Carrega o arquivo.
+  const data = await JSON.parse(await readFile(GRADE_FILE)); //Carrega o arquivo.
   const newGrade = req.body; //Carrega os parâmetros
   newGrade.id = data.nextId++; //Adiciona na nova grade o atual id e depois incrementa o próprio
   newGrade.timestamp = new Date(); //Adiciona o timestamp.
@@ -22,7 +22,7 @@ router.post("/", async (req, res) => {
   data.grades.push(orderedGrade); //Adiciona a nova grade junto as outras
   await writeFile("grades.json", JSON.stringify(data)); //Escreve tudo no arquivo
 
-  const newData = await JSON.parse(await readFile(gradeFile)); //Leia de novo o arquivo
+  const newData = await JSON.parse(await readFile(GRADE_FILE)); //Leia de novo o arquivo
   res.send(newData);
 });
 
@@ -35,7 +35,7 @@ router.post("/", async (req, res) => {
  * grades.json.
  */
 router.put("/", async (req, res) => {
-  const data = await JSON.parse(await readFile(gradeFile)); //Carrega os arquivo
+  const data = await JSON.parse(await readFile(GRADE_FILE)); //Carrega os arquivo
   const newGrade = req.body; //Carrega os parâmetros
   const gradeExist = data.grades.some((grade) => {
     return grade.id === newGrade.id;
@@ -52,7 +52,7 @@ router.put("/", async (req, res) => {
     };
   }
   await writeFile("grades.json", JSON.stringify(data));
-  const newData = await JSON.parse(await readFile(gradeFile));
+  const newData = await JSON.parse(await readFile(GRADE_FILE));
   res.send(newData);
 });
 
@@ -61,16 +61,72 @@ router.put("/", async (req, res) => {
  * parâmetro o id da grade e realizar sua exclusão do arquivo grades.json.
  */
 router.delete("/:id", async (req, res) => {
-  const data = await JSON.parse(await readFile(gradeFile));
+  const data = await JSON.parse(await readFile(GRADE_FILE));
   for (let i = 0; i < data.grades.length; i++) {
     if (data.grades[i].id == req.params.id) {
       data.grades.splice(i, 1); //Remove 1 elemento no indice "i"
       break;
     }
   }
-  await writeFile(gradeFile, JSON.stringify(data));
-  const newData = await JSON.parse(await readFile(gradeFile));
+  await writeFile(GRADE_FILE, JSON.stringify(data));
+  const newData = await JSON.parse(await readFile(GRADE_FILE));
   res.send(newData);
+});
+
+/**4. Crie um endpoint para consultar uma grade em específico. Este endpoint deverá
+ * receber como parâmetro o id da grade e retornar suas informações.
+ */
+router.get("/:id", async (req, res) => {
+  const data = await JSON.parse(await readFile(GRADE_FILE));
+  const grade = data.grades.find((grade) => grade.id == req.params.id);
+  res.send(grade);
+  // res.end(); //Termina execução
+});
+
+/**
+ * 5. Crie um endpoint para consultar a nota total de um aluno em uma disciplina. O
+ * endpoint deverá receber como parâmetro o student e o subject, e realizar a soma de
+ * todas os as notas de atividades correspondentes a aquele subject para aquele student. O
+ * endpoint deverá retornar a soma da propriedade value dos registros encontrados.
+ */
+// router.get("/", async (req, res) => {
+//   const data = await JSON.parse(await readFile(GRADE_FILE));
+//   const student = req.body.student;
+//   const subject = req.body.subject;
+//   let countOfSubject = 0;
+//   data.grades.forEach((grade) => {
+//     if (grade.student == student && grade.subject == subject) {
+//       countOfSubject += grade.value;
+//     }
+//   });
+//   res.send(
+//     `Student: ${student} || Subject: ${subject} || Total value: ${countOfSubject}!`
+//   );
+// });
+
+/**
+ * 6. Crie um endpoint para consultar a média das grades de determinado subject e type. O
+ * endpoint deverá receber como parâmetro um subject e um type, e retornar a média. A
+ * média é calculada somando o registro value de todos os registros que possuem o subject
+ * e type informados, e dividindo pelo total de registros que possuem este mesmo subject e
+ * type.
+ */
+router.get("/", async (req, res) => {
+  const data = await JSON.parse(await readFile(GRADE_FILE));
+  const subject = req.body.subject;
+  const type = req.body.type;
+  let sumOfValues = 0;
+  let countValues = 0;
+  for (let i = 0; i < data.grades.length; i++) {
+    if (data.grades[i].subject == subject && data.grades[i].type == type) {
+      sumOfValues += data.grades[i].value;
+      countValues++;
+    }
+  }
+  const avgValue = sumOfValues / countValues;
+  res.status(200).send(avgValue);
+  // console.log(avgValue);
+  // res.end();
 });
 
 router.use((req, res) => {
